5a9eb22e3b74e903f4447e5bfb46eccecf170e68
ALBERTO EMMANUEL TINAJERO VELAZCO <alberto.tinajero@iteso.mx>
2026-05-11 19:52:31 -0600

    refactor: clean duplicated folders and organize monorepo

--- src/routes/asignaturas.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { crearAsignatura, getAsignaturas } from '../controllers/asignaturas.controller.js';
-
-const router = express.Router();
-
-router.post('/', crearAsignatura);
-router.get('/', getAsignaturas);
-
-export default router;
--- src/utils/validators.js
+++
@@ -1,9 +0,0 @@
-// Validar email
-export const isValidEmail = (email) => {
-  return /^\S+@\S+\.\S+$/.test(email);
-};
-
-// Validar calificación
-export const isValidRating = (rating) => {
-  return rating >= 1 && rating <= 5;
-};
